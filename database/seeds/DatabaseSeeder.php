7209d0b1dff3e1eb51b6353821673ff099b7a6bb
BasharAlZawahreh <zawahrehbashar@gmail.com>
2022-03-12 11:41:45 +0300

    relations and seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Gender;
+use App\Models\Nationalitie;
+use App\Models\Religion;
+use App\Models\Specialization;
+use App\Models\Type_Blood;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,10 +19,62 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+
+        $this->call([
+            NationalitiesSeeder::class,
+        ]);
+
         User::factory()->create([
             'email' => 'a@a.com',
             'password' => '$2a$12$kk/1p/TlzK3D.0ybVGX5xOhxSWE0JWOfWMixSQgOiIQ85p/VtsXO6'
         ]);
+
+        $bgs = ['O-', 'O+', 'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-'];
+        foreach ($bgs as $bg) {
+
+            Type_Blood::create([
+                'name' => $bg
+            ]);
+        }
+
+        $genders = [
+            ['en'=> 'Male', 'ar'=> 'ذكر'],
+            ['en'=> 'Female', 'ar'=> 'انثي'],
+
+        ];
+        foreach ($genders as $ge) {
+            Gender::create(['name' => $ge]);
+        }
+
+        $specializations = [
+            ['en'=> 'Arabic', 'ar'=> 'عربي'],
+            ['en'=> 'Sciences', 'ar'=> 'علوم'],
+            ['en'=> 'Computer', 'ar'=> 'حاسب الي'],
+            ['en'=> 'English', 'ar'=> 'انجليزي'],
+        ];
+        foreach ($specializations as $S) {
+            Specialization::create(['Name' => $S]);
+        }
+
+        $religions = [
+
+            [
+                'en'=> 'Muslim',
+                'ar'=> 'مسلم'
+            ],
+            [
+                'en'=> 'Christian',
+                'ar'=> 'مسيحي'
+            ],
+            [
+                'en'=> 'Other',
+                'ar'=> 'غيرذلك'
+            ],
+
+        ];
+
+        foreach ($religions as $R) {
+            Religion::create(['name' => $R]);
+        }
     }
 }
